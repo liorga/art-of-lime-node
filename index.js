786571c826e81e6757f2957cb5ab8ededa7672fe
liorga <liorgal31892@gmail.com>
2024-03-01 15:21:16 +0200

    added cors handle to the app

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors');
 
 
+const corsOptions = {
+    origin: 'http://localhost:4200', // This should be the domain from which you expect to get requests
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  };
+  
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get('/', (req, res) => {
