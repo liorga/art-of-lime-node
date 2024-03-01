054ee92370721467f89b307b69256e70d0fc16cc
liorga <liorgal31892@gmail.com>
2024-03-01 15:03:25 +0200

    just minot test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello Art Of Lime');
 });
 
 app.listen(port, ()=>{
